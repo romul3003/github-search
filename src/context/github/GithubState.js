c80204994ba3794e3b136c5a8e5cdd21d948c8f3
Roman Zakrevskyi <romul3003@gmail.com>
2020-02-24 22:13:00 +0200

    Add GithubState.

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -1,6 +1,13 @@
 import React, { useReducer } from 'react'
 import { GithubContext } from './githubContext'
 import { githubReducer } from './githubReducer'
+import {
+	CLEAR_USERS,
+	GET_REPOS,
+	GET_USER,
+	SEARCH_USERS,
+	SET_LOADING,
+} from '../types'
 
 export const GithubState = ({ children }) => {
 	const initialState = {
@@ -9,8 +16,60 @@ export const GithubState = ({ children }) => {
 		loading: false,
 		repos: [],
 	}
-	// eslint-disable-next-line no-unused-vars
+
 	const [state, dispatch] = useReducer(githubReducer, initialState)
 
-	return <GithubContext.Provider value={{}}>{children}</GithubContext.Provider>
+	const search = async value => {
+		setLoading()
+		// ...
+
+		dispatch({
+			type: SEARCH_USERS,
+			payload: [],
+		})
+	}
+
+	const getUser = async name => {
+		setLoading()
+		// ...
+
+		dispatch({
+			type: GET_USER,
+			payload: {},
+		})
+	}
+
+	const getRepos = async name => {
+		setLoading()
+		// ...
+
+		dispatch({
+			type: GET_REPOS,
+			payload: [],
+		})
+	}
+
+	const clearUsers = () => dispatch({ type: CLEAR_USERS })
+
+	const setLoading = () => dispatch({ type: SET_LOADING })
+
+	const { user, users, repos, loading } = state
+
+	return (
+		<GithubContext.Provider
+			value={{
+				setLoading,
+				search,
+				getUser,
+				getRepos,
+				clearUsers,
+				user,
+				users,
+				repos,
+				loading,
+			}}
+		>
+			{children}
+		</GithubContext.Provider>
+	)
 }
