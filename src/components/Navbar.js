fbbdcdfdad17a7d87163520796c2b6eec176b40d
Roman Zakrevskyi <romul3003@gmail.com>
2020-02-24 15:04:25 +0200

    Add routing.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
 	return (
@@ -6,14 +7,14 @@ const Navbar = () => {
 			<div className="navbar-brand">Github search</div>
 			<ul className="navbar-nav">
 				<li className="nav-item">
-					<a href="/" className="nav-link">
+					<NavLink to="/" exact className="nav-link">
 						Home
-					</a>
+					</NavLink>
 				</li>
 				<li className="nav-item">
-					<a href="/about" className="nav-link">
+					<NavLink to="/about" className="nav-link">
 						Info
-					</a>
+					</NavLink>
 				</li>
 			</ul>
 		</nav>
--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const About = () => {
 	return (
-		<div>
-			<h1>About page</h1>
+		<div className="jumbotron">
+			<div className="container">
+				<h1 className="display-4">Info</h1>
+				<p className="lead">
+					App version: <strong>1.0.0</strong>
+				</p>
+			</div>
 		</div>
 	)
 }
