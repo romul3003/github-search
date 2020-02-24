d01ac3c7cdd12e72e2ea8388ebf6175dab5f3dd7
Roman Zakrevskyi <romul3003@gmail.com>
2020-02-24 23:04:45 +0200

    Add requests to github.com

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,11 @@
 import React, { useContext, useState } from 'react'
 import { AlertContext } from '../context/alert/alertContext'
+import { GithubContext } from '../context/github/githubContext'
 
 const Search = () => {
 	const [value, setValue] = useState('')
 	const { show } = useContext(AlertContext)
+	const github = useContext(GithubContext)
 
 	const onSubmit = event => {
 		if (event.key !== 'Enter') {
@@ -11,8 +13,7 @@ const Search = () => {
 		}
 
 		if (value.trim()) {
-			// eslint-disable-next-line no-console
-			console.log('Make request width: ', value)
+			github.search(value.trim())
 		} else {
 			show('Please, enter your data!', 'warning')
 		}
--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -1,4 +1,5 @@
 import React, { useReducer } from 'react'
+import axios from 'axios'
 import { GithubContext } from './githubContext'
 import { githubReducer } from './githubReducer'
 import {
@@ -8,6 +9,13 @@ import {
 	SEARCH_USERS,
 	SET_LOADING,
 } from '../types'
+
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET
+
+const widthCreds = url => {
+	return `${url}client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+}
 
 export const GithubState = ({ children }) => {
 	const initialState = {
@@ -21,31 +29,40 @@ export const GithubState = ({ children }) => {
 
 	const search = async value => {
 		setLoading()
-		// ...
+
+		const response = await axios.get(
+			widthCreds(`https://api.github.com/search/users?q=${value}&`)
+		)
 
 		dispatch({
 			type: SEARCH_USERS,
-			payload: [],
+			payload: response.data.items,
 		})
 	}
 
 	const getUser = async name => {
 		setLoading()
-		// ...
+
+		const response = await axios.get(
+			widthCreds(`https://api.github.com/users/${name}?`)
+		)
 
 		dispatch({
 			type: GET_USER,
-			payload: {},
+			payload: response.data,
 		})
 	}
 
 	const getRepos = async name => {
 		setLoading()
-		// ...
+
+		const response = await axios.get(
+			widthCreds(`https://api.github.com/users/${name}/repos?per_page=5&`)
+		)
 
 		dispatch({
 			type: GET_REPOS,
-			payload: [],
+			payload: response.data,
 		})
 	}
 
