d1d2b4ca78d703eb9c8a2a2789e3acc5ac94d842
Roman Zakrevskyi <romul3003@gmail.com>
2020-02-24 23:24:32 +0200

    Render home page.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Card = () => (
+const Card = ({ user }) => (
 	<div className="card">
-		<img src={''} alt="" className="card-img-top" />
+		<img src={user.avatar_url} alt={user.login} className="card-img-top" />
 		<div className="card-body">
-			<h5 className="card-title">React JS</h5>
+			<h5 className="card-title">{user.login}</h5>
 
-			{/* eslint-disable-next-line no-useless-concat */}
-			<Link to={'/profile/' + 'react'} className="btn btn-primary">
+			<Link to={'/profile/' + user.login} className="btn btn-primary">
 				Open
 			</Link>
 		</div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,7 @@ import { GithubContext } from '../context/github/githubContext'
 
 const Search = () => {
 	const [value, setValue] = useState('')
-	const { show } = useContext(AlertContext)
+	const alert = useContext(AlertContext)
 	const github = useContext(GithubContext)
 
 	const onSubmit = event => {
@@ -12,10 +12,13 @@ const Search = () => {
 			return
 		}
 
+		github.clearUsers()
+
 		if (value.trim()) {
+			alert.hide()
 			github.search(value.trim())
 		} else {
-			show('Please, enter your data!', 'warning')
+			alert.show('Please, enter your data!', 'warning')
 		}
 	}
 
