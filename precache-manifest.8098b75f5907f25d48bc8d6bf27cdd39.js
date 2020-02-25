9b7d1ff2248dd2a02aaa329ae22b9a4f9465a612
Roman Zakrevskyi <romul3003@gmail.com>
2020-02-25 14:48:39 +0200

    Updates

---
+++ precache-manifest.8098b75f5907f25d48bc8d6bf27cdd39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "339a9855931e8d9771698da12ea19ca8",
+    "url": "/github-search/index.html"
+  },
+  {
+    "revision": "723139d92bb67eade963",
+    "url": "/github-search/static/css/main.264016c4.chunk.css"
+  },
+  {
+    "revision": "525613ea4b6e7154b402",
+    "url": "/github-search/static/js/2.de101b77.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/github-search/static/js/2.de101b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "723139d92bb67eade963",
+    "url": "/github-search/static/js/main.3117a82c.chunk.js"
+  },
+  {
+    "revision": "09cfeb4ff2540d50b1d9",
+    "url": "/github-search/static/js/runtime-main.d1e48445.js"
+  }
+]);
